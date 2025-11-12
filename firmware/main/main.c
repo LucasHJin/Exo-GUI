bc151154712255f76fbb96a1de66355ad0fde0f3
Sarah Yoo <gaeunyoo3626@gmail.com>
2025-11-12 13:00:51 -0500

    add O S1 drive api functions

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -15,9 +15,15 @@ Also, you must open 'this' projct in vs code, not via Exo_GUI. Otherwie vs code 
 #define rx_gpio GPIO_NUM_1
 
 void send_msg(uint32_t id, uint8_t *data, uint8_t length, bool extended, uint32_t timeout_ms);
-twai_message_t* receive_msg(uint32_t timeout_ms);
+twai_message_t receive_msg(uint32_t timeout_ms);
 void stop_driver();
 void uninstall_driver();
+void set_pos(float pos, float vel, float torq, float vel_scale, float torq_scale, uint32_t timeout_ms);
+void set_pos_default(float pos, float vel, float torq, uint32_t timeout_ms);
+void set_vel(float vel, float torq, uint32_t timeout_ms);
+void set_torque(float torq, uint32_t timeout_ms);
+float get_pos_estimate(uint32_t timeout_ms);
+float get_vel_estimate(uint32_t timeout_ms);
 
 void app_main(void)
 {
@@ -42,7 +48,10 @@ void app_main(void)
         return;
     }
 
-    //TODO: send data in a loop or so
+    //TODO: send command in a loop
+    while (1) {
+        
+    }
 }
 
 /**
@@ -50,7 +59,7 @@ void app_main(void)
  * 
  * @param id       CAN ID (standard 11-bit or extended 29-bit)
  * @param data     Pointer to data bytes to send
- * @param length   Number of bytes in data (0-8)
+ * @param length   Number of bytes in data (0-8) (size of data)
  * @param extended  0 = standard frame, 1 = extended frame
  * @param timeout_ms Timeout in milliseconds for sending
  */
@@ -60,31 +69,28 @@ void send_msg(uint32_t id, uint8_t *data, uint8_t length, bool extended, uint32_
     msg.identifier = id;
     msg.extd = extended;
     msg.data_length_code = length;
-
-    for (int i = 0; i < length; i ++) {
-        msg.data[i] = data[i];
-    }
+    memcpy(msg.data, data, length);
 
     //queue message for transmission
     esp_err_t res = twai_transmit(&msg, pdMS_TO_TICKS(timeout_ms));
     if (res == ESP_OK) {
-        ESP_LOGI("CAN message sent (ID: 0x%03X, DLC: %d)", id, length);
+        printf("CAN message sent (ID: %d, len: %d)", id, length);
     } else {
-        ESP_LOGE("Failed to send CAN message (ID: 0x%03X)", id);
+        printf("Failed to send CAN message (ID: %d)", id);
     }
 }
 
-twai_message_t* receive_msg(uint32_t timeout_ms) {
+twai_message_t receive_msg(uint32_t timeout_ms) {
     //wait for message to be received
     twai_message_t msg;
     esp_err_t res = twai_receive(&msg, pdMS_TO_TICKS(timeout_ms));
         
     if (res != ESP_OK) {
         printf("Failed to receive message\n");
-        return NULL;
+        msg.identifier = -1;
     }
 
-    return &msg;
+    return msg;
 }
 
 void stop_driver() {
@@ -101,4 +107,65 @@ void uninstall_driver() {
     } else {
         printf("Failed to uninstall driver\n");
     }
+}
+
+void set_pos(float pos, float vel, float torq, float vel_scale, float torq_scale, uint32_t timeout_ms) {
+    uint8_t data[8]; //always send 8 bytes data
+
+    //allocate pos float into first 4 bytes of data (float is 4 bytes)
+    memcpy(&data[0], &pos, 4);
+
+    //desired feedforward velocity
+    int16_t vel_ff = (int16_t) (vel * vel_scale);
+    //split 16 bits into 8 bits
+    data[4] = vel_ff & 0xFF; //store the lower 8 bits
+    data[5] = (vel_ff >> 8) & 0xFF; //shift 8 bits to the right and store the upper 8 bits
+
+    //desired feedforward torque
+    int16_t torq_ff = (int16_t) (torq * torq_scale);
+    data[6] = torq_ff & 0xFF;
+    data[7] = (torq_ff >> 8) & 0xFF;
+
+    send_msg(0x0c, data, 8, false, timeout_ms);
+}
+
+void set_pos_default(float pos, float vel, float torq, uint32_t timeout_ms) {
+    set_pos(pos, vel, torq, 1.0, 1.0, timeout_ms);
+}
+
+void set_vel(float vel, float torq, uint32_t timeout_ms) {
+    uint8_t data[8]; 
+    memcpy(&data[0], &vel, 4); //float is 4 bytes
+    memcpy(&data[4], &torq, 4);
+    send_msg(0x0d, data, 8, false, timeout_ms);
+}
+
+void set_torque(float torq, uint32_t timeout_ms) {
+    uint8_t data[8];
+    memcpy(&data[0], torq, 4);
+    send_msg(0x0e, data, 8, false, timeout_ms);
+}
+
+float get_pos_estimate(uint32_t timeout_ms) {
+    //send reqeust for estimated encoder pos value
+    send_msg(0x09, NULL, 0, false, timeout_ms);
+
+    //receive message
+    twai_message_t rx = receive_msg(timeout_ms);
+    if (rx.identifier == -1) return 0.0;
+
+    float pos;
+    memcpy(&pos, rx.data, 4);
+    return pos;
+}
+
+float get_vel_estimate(uint32_t timeout_ms) {
+    send_msg(0x09, NULL, 0, false, timeout_ms);
+
+    twai_message_t rx = receive_msg(timeout_ms);
+    if (rx.identifier == -1) return 0.0;
+
+    float vel;
+    memcpy(&vel, rx.data, 4);
+    return vel;
 }
